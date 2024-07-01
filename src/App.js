e9b3e806e17e8310b3b9494439aa7263790a67ea
hadees <hadees621@gmail.com>
2024-07-01 15:20:13 +0500

    chore: tailwind added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">ADMIN PANEL</div>;
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold underline pt-24 text-red-300">
+        Hello world!
+      </h1>
+    </div>
+  );
 }
 
 export default App;
