685451598de96eb2fc5bb4ad5b33b2a546c91447
hadees <hadees621@gmail.com>
2024-07-02 18:47:15 +0500

    feature: sidebar controls

---
+++ src/components/sidebar/components/SmallLogo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SmallLogo = () => {
+  return (
+    <a href="/">
+      <img
+        src="/assets/logomark.png"
+        alt="small logo"
+        className="w-[400px] h-[400px] align-middle p-2 border"
+      />
+    </a>
+  );
+};
+
+export default SmallLogo;
