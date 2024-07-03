4ca59497c3e17875bb368ff533cb8628e54495aa
hadees <hadees621@gmail.com>
2024-07-03 12:59:50 +0500

    contract: dpne

--- src/pages/contract/Contract.js
+++ src/pages/contract/Contract.js
@@ -3,6 +3,7 @@ import Plus from "utils/icons/Plus";
 import Button from "components/Button";
 import SelectField from "components/SelectField";
 import SearchField from "pages/home/components/SearchField";
+import { authorInfo, contractInfoLeft, contractInfoRight } from "./utils/utils";
 
 const Contract = () => {
   const [selectedValues, setSelectedValues] = useState(["", ""]);
@@ -22,13 +23,69 @@ const Contract = () => {
 
   const placeholders = ["Please Select Author", "Select Book"];
 
+  const InformationSection = ({ title, info, imageUrl }) => (
+    <div className="w-full shadow-lg">
+      <p className="text-xl font-bold mx-5 mt-4">{title}</p>
+      <div className="flex justify-between p-8">
+        <div className="my-4 gap-4 grid">
+          {info.map((item, index) => (
+            <div key={index}>
+              <p className="text-sm font-semibold text-gray-600">
+                {item.label}
+              </p>
+              <p className="text-lg font-semibold text-black">{item.value}</p>
+            </div>
+          ))}
+        </div>
+        {imageUrl && (
+          <div>
+            <p className="text-sm font-semibold text-gray-600">
+              Author Picture:
+            </p>
+            <img src={imageUrl} alt="author" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+
+  const ContractInformation = ({ infoLeft, infoRight }) => (
+    <div className="w-full shadow-lg">
+      <p className="text-xl font-bold m-3">Contract Information</p>
+      <div className="flex justify-between gap-14 p-8">
+        <div className="w-full grid gap-4">
+          {infoLeft.map((item, index) => (
+            <div key={index}>
+              <p className="text-sm font-semibold text-gray-600">
+                {item.label}
+              </p>
+              <p className="text-md font-semibold text-black max-w-sm">
+                {item.value}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="w-full grid gap-4">
+          {infoRight.map((item, index) => (
+            <div key={index}>
+              <p className="text-sm font-semibold text-gray-600">
+                {item.label}
+              </p>
+              <p className="text-md font-semibold text-black">{item.value}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <div className="w-full text-start items-center">
       <p className="text-3xl font-semibold mt-8 ml-8">
         Welcome to Olympia Portal (olympia admin)
       </p>
 
-      <div className="flex items-center mt-8 gap-3 ml-5 pr-10">
+      <div className="flex items-center mt-8 gap-3 m-4">
         <SearchField placeholder="Book Title" />
         <SearchField placeholder="Pen name/Author name" />
         <Button title="Search" text="text-[12px]" />
@@ -36,7 +93,7 @@ const Contract = () => {
         <Button title="ISBN Search" text="text-[12px]" />
       </div>
 
-      <div className="flex gap-2 justify-start mt-3 items-center ml-5 w-full pr-10">
+      <div className="flex gap-2 justify-start mt-3 items-center w-full px-4">
         <p className="text-md font-semibold text-gray-500">Pen Name:</p>
         {placeholders.map((placeholder, index) => (
           <SelectField
@@ -54,7 +111,7 @@ const Contract = () => {
         <Button title="Author Data Sheet" text="text-[12px]" />
       </div>
 
-      <div className="shadow mt-4 bg-[#F7F7F7] p-3 flex justify-between m-4 items-center px-">
+      <div className="shadow mt-4 bg-[#F7F7F7] p-3 flex justify-between m-4 items-center">
         <p className="text-md font-semibold text-gray-500">
           Aine By Yugtha YJ <span className="text-black"> Book No. </span> 1
         </p>
@@ -64,7 +121,17 @@ const Contract = () => {
         </div>
       </div>
 
-      <div></div>
+      <div className="m-4 flex gap-3 mt-8">
+        <InformationSection
+          title="Author Information"
+          info={authorInfo}
+          imageUrl="https://picsum.photos/200/300?grayscale"
+        />
+        <ContractInformation
+          infoLeft={contractInfoLeft}
+          infoRight={contractInfoRight}
+        />
+      </div>
     </div>
   );
 };
---
+++ src/pages/contract/utils/utils.js
@@ -0,0 +1,23 @@
+export const authorInfo = [
+  { label: "Author Name:", value: "Miss. Yajna Yugtha Jungbadoor" },
+  { label: "Pen Name:", value: "Yugtha YJ" },
+  { label: "Date of Birth:", value: "NA" },
+];
+
+export const contractInfoLeft = [
+  { label: "Book Title:", value: "Aine" },
+  { label: "ISBN:", value: "978-1-78830-014-8" },
+  { label: "Book No:", value: "1" },
+  { label: "Ebook:", value: "UnPublished" },
+  {
+    label: "Important Note",
+    value:
+      "loremipsum dolor sit amet, consectetur adipiscing loremipsum amet, consectetur adipiscing",
+  },
+];
+
+export const contractInfoRight = [
+  { label: "Contract Date:", value: "03-04-2017" },
+  { label: "Pen Name:", value: "Yugtha YJ" },
+  { label: "Date of Birth:", value: "NA" },
+];
