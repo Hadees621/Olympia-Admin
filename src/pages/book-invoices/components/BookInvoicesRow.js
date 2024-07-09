5bd609102220625484c886dfddc1449a81f2b935
hadees <hadees621@gmail.com>
2024-07-09 14:45:16 +0500

    fixes

--- src/pages/book-invoices/components/BookInvoicesRow.js
+++ src/pages/book-invoices/components/BookInvoicesRow.js
@@ -1,16 +1,22 @@
 import TableButton from "components/TableButton";
 import React from "react";
 
-const Row = () => {
+const BookInvoicesRow = () => {
   return (
     <tr className="text-[11px] border-b text-center">
-      <td className="p-3">08-06-2010</td>
-      <td className="p-3">2107</td>
+      <td className="p-3">28-10-2010</td>
+      <td className="p-3">2593</td>
+      <td className="p-3">56793</td>
       <td className="p-3">0000-00-00</td>
-      <td className="p-3">Amazon.co.uk (oly)</td>
-      <td className="p-3">£ 3.20</td>
-      <td className="p-3">Amazon.co.uk </td>
-      <td className="p-3"> Yes</td>
+      <td className="p-3">90</td>
+      <td className="p-3"></td>
+
+      <td className="p-3">£ 359.55</td>
+      <td className="p-3"></td>
+      <td className="p-3">359.55</td>
+      <td className="p-3">Author Sale</td>
+      <td className="p-3">Royalties</td>
+      <td className="p-3"> Actions</td>
       <td className="p-3">
         <div className="flex justify-center items-center space-x-2">
           <TableButton title={"View"} />
@@ -22,4 +28,4 @@ const Row = () => {
   );
 };
 
-export default Row;
+export default BookInvoicesRow;
