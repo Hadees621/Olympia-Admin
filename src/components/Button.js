1ea6f936a12ec46b43b824fee11112a29ef496a2
hadees <hadees621@gmail.com>
2024-07-23 12:55:15 +0500

    chore: /author edit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Button = ({ title, text, bg = "bg-[#001C4E1F]", href = "#", onclick, width }) => {
+const Button = ({ title, text, bg = "bg-[#001C4E1F]", href = "#", onClick, width }) => {
   const textClass = text;
   const backgroundColor = bg;
 
   return (
     <Link to={href}>
       <button
-        onClick={onclick}
+        onClick={onClick}
         className={`${backgroundColor} ${textClass} ${width} rounded-md text-[#001C4E] font-bold px-8 py-2 text-[12px]`}
       >
         {title}
