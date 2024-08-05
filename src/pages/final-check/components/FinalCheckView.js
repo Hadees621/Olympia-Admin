66cdfdeb7b8a7d296d6545ab53696621d828f6e6
hadees <hadees621@gmail.com>
2024-08-05 19:15:05 +0500

    fix

--- src/pages/final-check/components/FinalCheckView.js
+++ src/pages/final-check/components/FinalCheckView.js
@@ -40,22 +40,11 @@ const FinalCheckView = () => {
     ];
 
     return (
-
         <div className="w-full text-start items-center m-4">
             {/* header */}
-            <>
-                <p className="text-3xl font-semibold mt-8 ml-8">
-                    Welcome to Olympia Portal (olympia admin)
-                </p>
-
-                <div className="flex items-center mt-3 gap-3 m-4">
-                    <SearchField placeholder="Book Title" />
-                    <SearchField placeholder="Pen name/Author name" />
-                    <Button title="Search" />
-                    <Button title="Clear" />
-                    <Button title="ISBN Search" href="/isbn-search" />
-                </div>
-            </>
+            <p className="text-3xl font-semibold mt-8 ml-">
+                Welcome to Olympia Portal (olympia admin)
+            </p>
 
             <Remaindered status={"Paid-in-full"} />
 
@@ -90,10 +79,8 @@ const FinalCheckView = () => {
                 </table>
             </div>
             <SelectInputWithLabel label={"Checked By*"} width="max-w-md" />
-
             <FinalCheckHistory history={finalCheckHistoryData} />
         </div>
-
     );
 };
 
