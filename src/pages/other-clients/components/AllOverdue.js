f546b82deda9a43b06cbf6a9ea0f3b9c8d0454e6
hadees <hadees621@gmail.com>
2024-08-08 15:36:44 +0500

    fix

--- src/pages/other-clients/components/AllOverdue.js
+++ src/pages/other-clients/components/AllOverdue.js
@@ -35,7 +35,7 @@ const AllOverdue = () => {
       className={`m-4 transition-all duration-300 ${isOpen ? "max-w-[1050px]" : "max-w-[1200px]"
         }`}
     >
-      <div className="flex items-center mt-8 gap-3 m-4 justify-end">
+      <div className="flex items-center mt-8 gap-3 my-4 justify-end">
         <Button
           title={"Add New Client"}
           bg="bg-green-500"
