5c086461658b8eb89e209c8b6436cd35e1f64f9e
hadees <hadees621@gmail.com>
2024-08-19 18:58:13 +0500

    fix: .login page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
+import "./index.css";
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
-import "./index.css";
 import Sidebar from "./components/sidebar/Sidebar";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
+
+const AppWithSidebar = () => {
+  const location = useLocation();
+  const hideSidebar = location.pathname === "/login";
+
+  return (
+    <div className="flex">
+      {!hideSidebar && <Sidebar />}
+      <App />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <div className="flex">
-        <Sidebar />
-        <App />
-      </div>
+      <AppWithSidebar />
     </Router>
   </React.StrictMode>
 );
