cba18bee28c41404976afdea389ea3438cf4a792
hadees <hadees621@gmail.com>
2024-08-19 19:39:52 +0500

    /login

--- src/components/sidebar/components/LogoutButton.js
+++ src/components/sidebar/components/LogoutButton.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const LogoutButton = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/login");
+  };
+
   return (
     <button
-      className="rounded-3xl m-2 shadow-lg py-2 font-bold bg-red-500 hover:bg-red-600 text-white hover:border-none
-    hover:border border-[#f1eded] text-[14px]"
+      onClick={handleLogout}
+      className="rounded-3xl m-2 shadow-lg py-2 font-bold bg-red-500 hover:bg-red-600 text-white hover:border-none border-[#f1eded] text-[14px]"
     >
       Logout
     </button>
