112d92346ff62e1d88cdbd6ca7eab8299e91a8d9
hadees <hadees621@gmail.com>
2024-08-19 19:44:56 +0500

    fixes

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,61 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Button from 'components/Button';
 
 const Login = () => {
     return (
-        <div className='w-full'>
-            <section className="bg-gray-50">
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen">
-                    <div>
-                        <img
-                            src="/assets/logo.png"
-                            alt="logo"
-                            className="w-[210px] align-middle p-4"
-                        />
-                    </div>
-                    <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-                        <div className="px-6 py-8 space-y-7">
-                            <p className="text-3xl font-semibold">
-                                Welcome to Olympia Portal
-                            </p>
-                            <form className="space-y-4 md:space-y-6" action="#">
-                                <div>
-                                    <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900">Username</label>
-                                    <input
-                                        type="text"
-                                        name="username"
-                                        id="username"
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                                        required
-                                    />
-                                </div>
-                                <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        id="password"
-                                        placeholder="••••••••"
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                                        required
-                                    />
-                                </div>
-                                <div className=''>
-                                    <Link to={"/"} >
-                                        <button
-                                            type="submit"
-                                            className="w-full text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                                        >
-                                            Sign in
-                                        </button>
-                                    </Link>
-                                </div>
-                            </form>
-                        </div>
+        <section className="bg-gray-50 w-full">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen">
+                <div>
+                    <img
+                        src="/assets/logo.png"
+                        alt="logo"
+                        className="w-[210px] align-middle p-4"
+                    />
+                </div>
+                <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="px-6 py-8 space-y-7">
+                        <p className="text-3xl font-semibold">
+                            Welcome to Olympia Portal
+                        </p>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label htmlFor="username" className="block mb-2 text-md font-bold text-gray-900">Username</label>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    id="username"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-md font-bold text-gray-900">Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    required
+                                />
+                            </div>
+                            <div className=''>
+                                <Button title={"Login"} width={"w-full"} bg='bg-green-600' hover='hover:bg-green-700' text={"text-white"} href='/' />
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     );
 };
 
