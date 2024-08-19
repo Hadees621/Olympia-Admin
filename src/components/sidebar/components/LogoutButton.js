35963d50527ab46682858d9a287d0b7048065c31
hadees <hadees621@gmail.com>
2024-08-19 17:20:57 +0500

    fix: sidebar

--- src/components/sidebar/components/LogoutButton.js
+++ src/components/sidebar/components/LogoutButton.js
@@ -3,8 +3,8 @@ import React from "react";
 const LogoutButton = () => {
   return (
     <button
-      className="rounded-3xl m-2 shadow-lg p-2 font-medium bg-red-400 hover:bg-red-500 text-white hover:border-none
-    hover:border border-[#f1eded] text-[12px]"
+      className="rounded-3xl m-2 shadow-lg py-2 font-bold bg-red-500 hover:bg-red-600 text-white hover:border-none
+    hover:border border-[#f1eded] text-[14px]"
     >
       Logout
     </button>
---
+++ src/utils/icons/CloseCross.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CloseCross = () => {
+    return (
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+                <path fill="none" stroke="black" stroke-linecap="round" d="m6 6l12 12m0-12L6 18" />
+            </svg>
+        </div>
+    )
+}
+
+export default CloseCross
