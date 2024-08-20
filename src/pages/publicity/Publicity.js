c02ba027521f8983ed893ca460a4b15a8831dce9
hadees <hadees621@gmail.com>
2024-08-20 18:31:10 +0500

    /publicity

--- src/pages/publicity/Publicity.js
+++ src/pages/publicity/Publicity.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import Title from "components/Title";
 import Button from "components/Button";
+import React, { useState } from "react";
 import SelectField from "components/SelectField";
 import SearchField from "pages/home/components/SearchField";
 import Remaindered from "../editorial/components/Remaindered";
@@ -25,39 +26,37 @@ const Publicity = () => {
     setSelectedValues(newSelectedValues);
   };
 
-  const placeholders = ["Please Select Author", "Select Book"];
-
   const handleSave = (formData) => {
     console.log("Saved data:", formData);
   };
 
+  const placeholders = ["Please Select Author", "Select Book"];
+
   return (
     <div className="w-full text-start items-center m-4">
-      {/* header */}
-      <>
-        <p className="text-3xl font-semibold mt-8">
-          Welcome to Olympia Portal (olympia admin)
-        </p>
-        <div className="flex items-center mt-8 gap-3">
-          <SearchField placeholder="Book Title" />
-          <SearchField placeholder="Pen name/Author name" />
-          <Button title="Search" />
-          <Button title="Clear" />
-          <Button title="ISBN Search" href="/isbn-search" />
-        </div>
-        <div className="flex gap-2 justify-start mt-3 items-center w-full">
-          <p className="text-md font-semibold text-gray-500">Pen Name:</p>
-          {placeholders.map((placeholder, index) => (
-            <SelectField
-              key={index}
-              placeholder={placeholder}
-              value={selectedValues[index]}
-              onChange={(e) => handleSelectChange(index, e)}
-            />
-          ))}
-          <Button title="Clear" />
-        </div>
-      </>
+
+      <Title />
+
+      <div className="flex items-center mt-8 gap-3">
+        <SearchField placeholder="Book Title" />
+        <SearchField placeholder="Pen name/Author name" />
+        <Button title="Search" />
+        <Button title="Clear" />
+        <Button title="ISBN Search" href="/isbn-search" />
+      </div>
+
+      <div className="flex gap-2 justify-start mt-3 items-center w-full">
+        <p className="text-md font-semibold text-gray-500">Pen Name:</p>
+        {placeholders.map((placeholder, index) => (
+          <SelectField
+            key={index}
+            placeholder={placeholder}
+            value={selectedValues[index]}
+            onChange={(e) => handleSelectChange(index, e)}
+          />
+        ))}
+        <Button title="Clear" />
+      </div>
 
       <Remaindered />
 
