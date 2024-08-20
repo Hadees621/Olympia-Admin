d388970f2cec904b890d4d7ff69bfbbf2f8ecfa6
hadees <hadees621@gmail.com>
2024-08-20 18:50:19 +0500

    /purchases

--- src/pages/purchases/utils/utils.js
+++ src/pages/purchases/utils/utils.js
@@ -1,67 +1,195 @@
 export const data = [
-    {
-      name: "John",
-      lastName: "Doe",
-      company: "Example Corp",
-      vat: "123456789",
-      tel: "123-456-7890",
-      mobile: "098-765-4321",
-      emails: "john@example.com",
-      skype: "john.doe",
-      website: "https://example.com",
-      address1:
-        "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
-      address2:
-        "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
-      signupDate: "2022-01-01",
-    },
-    {
-      name: "John",
-      lastName: "Doe",
-      company: "Example Corp",
-      vat: "123456789",
-      tel: "123-456-7890",
-      mobile: "098-765-4321",
-      emails: "john@example.com",
-      skype: "john.doe",
-      website: "https://example.com",
-      address1:
-        "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
-      address2:
-        "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
-      signupDate: "2022-01-01",
-    },
-    {
-      name: "John",
-      lastName: "Doe",
-      company: "Example Corp",
-      vat: "123456789",
-      tel: "123-456-7890",
-      mobile: "098-765-4321",
-      emails: "john@example.com",
-      skype: "john.doe",
-      website: "https://example.com",
-      address1:
-        "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
-      address2:
-        "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
-      signupDate: "2022-01-01",
-    },
-    {
-      name: "John",
-      lastName: "Doe",
-      company: "Example Corp",
-      vat: "123456789",
-      tel: "123-456-7890",
-      mobile: "098-765-4321",
-      emails: "john@example.com",
-      skype: "john.doe",
-      website: "https://example.com",
-      address1:
-        "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
-      address2:
-        "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
-      signupDate: "2022-01-01",
-    },
-    // Add more rows as needed
-  ];
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+  {
+    name: "John",
+    lastName: "Doe",
+    company: "Example Corp",
+    vat: "123456789",
+    tel: "123-456-7890",
+    mobile: "098-765-4321",
+    emails: "john@example.com",
+    skype: "john.doe",
+    website: "https://example.com",
+    address1:
+      "123 Example Street123 Example Street123 Examtreet123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street ",
+    address2:
+      "123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example Street123 Example23 Example Street123 Example Street123 Example Street ",
+    signupDate: "2022-01-01",
+  },
+
+];
