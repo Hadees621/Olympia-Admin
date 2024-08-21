bb13a64b37a4229dce848add4626616a8ad73c1d
hadees <hadees621@gmail.com>
2024-08-21 15:08:54 +0500

    /isbn-search

--- src/pages/nested-pages/isbn-search/IsbnSearch.js
+++ src/pages/nested-pages/isbn-search/IsbnSearch.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Title from "components/Title";
 import Button from "components/Button";
 import SummaryTable from "./components/SummaryTable";
 import DetailedTable from "./components/DetailedTable";
@@ -12,22 +13,19 @@ import {
 
 const IsbnSearch = () => {
   return (
-    <div className="w-full text-start items-center">
-      {/* header */}
-      <>
-        <p className="text-3xl font-semibold mt-8 ml-8">
-          Welcome to Olympia Portal (olympia admin)
+    <div className="w-full text-start items-center m-4">
+      <Title />
+
+      <div className="flex gap-2 justify-start items-center w-[43%] mx-auto my-6">
+        <p className="text-md font-semibold text-gray-500">
+          Search by ISBN :
         </p>
-        <div className="flex gap-2 justify-start items-center w-[43%] mx-auto px-4 my-6">
-          <p className="text-md font-semibold text-gray-500">
-            Search by ISBN :
-          </p>
-          <SearchField placeholder="000-0-0000000-00-0" />
-          <Button title="Search" />
-          <Button title="Clear" />
-        </div>
-      </>
-      <div className="mt-4 m-4 font-semibold text-lg grid grid-cols-2 gap-4">
+        <SearchField placeholder="000-0-0000000-00-0" />
+        <Button title="Search" />
+        <Button title="Clear" />
+      </div>
+      
+      <div className="mt-4 font-semibold text-lg grid grid-cols-2 gap-4">
         <SummaryTable title="Available ISBN (Press)" data={availableISBNData} />
         <SummaryTable title="Used ISBN (Press)" data={usedISBNData} />
         <DetailedTable title="Total Available ISBN" data={totalAvailableISBN} />
